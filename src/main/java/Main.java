a05d2b2096d63d440e060ae76a3ff2d684fe7bfd
Masterkaj2137 <148704613+Masterkaj2137@users.noreply.github.com>
2025-11-03 08:18:58 +0000

    Fortnite

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    for(int k = 0; k<5; k++){
-      for(int i = 0; i<5; i++){
-      System.out.print("*");
-      }
-      System.out.println("");
-    }
-  }
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.print("Podaj rozmiar kwadratu: ");
+    int n = scanner.nextInt();
+
+    for (int k = 0; k < n; k++) {
+      for (int i = 0; i < n; i++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+    scanner.close();
+  }
 }
