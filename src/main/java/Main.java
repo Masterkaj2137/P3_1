7895ea2f0edfc30d2b3731aa62f8e1e4a4bd9679
Masterkaj2137 <148704613+Masterkaj2137@users.noreply.github.com>
2025-11-03 08:09:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    for(int k = 0; k<5; k++){
+      for(int i = 0; i<5; i++){
+      System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+
+}
